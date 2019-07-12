d9d6144f039cd204fe8cc237ac634b2fa38ccf41
barzur <boris@konbu.org>
2019-07-12 11:33:32 +0900

    usb/dwc2/hcd: channel halt may not be an error
    Truncating the data was causing null pointer exceptions in memcpy,
    let's be more careful.
    
    Addendum by Urja Rannikko:
    
    This was reported as a pull request on my linux github repo and it came
    with this reproducer & notes:
    -- begin --
    I sometimes get kernel oops when USB tethering.
    Here is how to repro:
    * create high load on the CPU: I build a kernel with -j6;
    * saturate your cdc_ether connection to the phone (I use netcat on both sides
      to do so, with termux on android);
    
    I tracked down the issue to buffers getting truncated, the driver extracting
    a target address from the end of the buffer (weird) & memcpy to a non validated
    address.
    
    I've fixed it by validating the address we write to & stopped treating
    un-explained channel closure as errors. I guess the channel is closed due to a
    timeout (maybe?), caused by heavy context switching on the machine.
    
    I've been testing the following patch for ~1 week and it seems to work well.
    -- end --
    
    I do not have a suitable phone to reproduce this with so I cant personally
    say if it works and I dont 100% understand it, but I looked at the patch
    and I think it looks okay-ish (tho maybe should be two patches), so in the
    meantime I'm including it here.

--- drivers/usb/dwc2/hcd_intr.c
+++ drivers/usb/dwc2/hcd_intr.c
@@ -1935,7 +1935,8 @@ static void dwc2_hc_chhltd_intr_dma(struct dwc2_hsotg *hsotg,
 					"hcint 0x%08x, intsts 0x%08x\n",
 					chan->hcint,
 					dwc2_readl(hsotg, GINTSTS));
-				goto error;
+				dwc2_halt_channel(hsotg, chan, qtd,
+					DWC2_HC_XFER_PERIODIC_INCOMPLETE);
 			}
 		}
 	} else {
