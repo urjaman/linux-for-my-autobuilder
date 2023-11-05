cff88c920f502ac2c2fd9019fc3479f4ec6302eb
Alex Bee <knaerzche@gmail.com>
2023-11-05 23:36:11 +0000

    media: rkvdec: Add RK3288 variant
    Add a RK3288 variant, a version of the Rockchip VDEC IP that only
    support HEVC decoding.
    
    Signed-off-by: Alex Bee <knaerzche@gmail.com>
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- drivers/staging/media/rkvdec/rkvdec.c
+++ drivers/staging/media/rkvdec/rkvdec.c
@@ -1203,6 +1203,10 @@ static void rkvdec_watchdog_func(struct work_struct *work)
 	}
 }
 
+static const struct rkvdec_variant rk3288_rkvdec_variant = {
+	.capabilities = RKVDEC_CAPABILITY_HEVC,
+};
+
 static const struct rkvdec_variant rk3399_rkvdec_variant = {
 	.capabilities = RKVDEC_CAPABILITY_HEVC |
 			RKVDEC_CAPABILITY_H264 |
@@ -1210,6 +1214,10 @@ static const struct rkvdec_variant rk3399_rkvdec_variant = {
 };
 
 static const struct of_device_id of_rkvdec_match[] = {
+	{
+		.compatible = "rockchip,rk3288-vdec",
+		.data = &rk3288_rkvdec_variant,
+	},
 	{
 		.compatible = "rockchip,rk3399-vdec",
 		.data = &rk3399_rkvdec_variant,
